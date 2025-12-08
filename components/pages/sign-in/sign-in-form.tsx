6033db82c70d626bf31be19fe195f552fec64c56
CodifyCanvas <shahzaibawan1357@gmail.com>
2025-12-08 12:20:32 +0500

    create forms for sign-in & sign-up using the shadcn-ui Field component

--- components/pages/sign-in/sign-in-form.tsx
+++ components/pages/sign-in/sign-in-form.tsx
@@ -1,15 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-import {
-  Form,
-  FormField,
-  FormControl,
-} from "@/components/ui/form";
 
 import {
   Field,
@@ -24,9 +18,15 @@ import { Button } from "@/components/ui/button";
 import { SignInFormSchema } from "@/lib/zod/schema.zod";
 import Link from "next/link";
 import toast from "react-hot-toast";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 export default function SignInForm() {
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const form = useForm<z.infer<typeof SignInFormSchema>>({
     resolver: zodResolver(SignInFormSchema),
@@ -38,78 +38,91 @@ export default function SignInForm() {
     await new Promise((r) => setTimeout(r, 1500));
     setLoading(false);
     console.log(values);
-    toast.success("Login Success")
+    toast.success("Login Success");
   }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="max-w-80 w-full mx-auto"
-      >
-        <FieldSet>
-          <FieldGroup>
+    <form
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="max-w-80 w-full mx-auto"
+    >
+      <FieldSet>
+        <FieldGroup>
 
-            {/* EMAIL */}
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field, fieldState }) => (
-                <Field data-invalid={!!fieldState.error}>
-                  <FieldLabel htmlFor="email">Email</FieldLabel>
+          {/* EMAIL */}
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={!!fieldState.error}>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  placeholder="example123@xyz.com"
+                  variant="primary"
+                  className="h-14"
+                  {...field}
+                />
+                <FieldError errors={fieldState.error ? [fieldState.error] : []} />
+              </Field>
+            )}
+          />
 
-                  <FormControl>
-                    <Input
-                      id="email"
-                      placeholder="example123@xyz.com"
-                      variant="primary"
-                      className="h-14"
-                      {...field}
-                    />
-                  </FormControl>
+          {/* PASSWORD */}
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={!!fieldState.error}>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <div className="relative w-full">
+                  <Input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder={showPassword ? "Password" : "••••••"}
+                    variant="primary"
+                    className="h-14 pr-12"
+                    {...field}
+                  />
 
-                  <FieldError errors={fieldState.error ? [fieldState.error] : []} />
-                </Field>
-              )}
-            />
+                  <Button
+                    type="button"
+                    variant="link"
+                    onClick={togglePasswordVisibility}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+                  >
+                    {showPassword
+                      ? <EyeOffIcon className="h-5 w-5 text-muted-foreground" />
+                      : <EyeIcon className="h-5 w-5 text-muted-foreground" />
+                    }
+                  </Button>
+                </div>
+                <FieldError errors={fieldState.error ? [fieldState.error] : []} />
+              </Field>
+            )}
+          />
 
-            {/* PASSWORD */}
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field, fieldState }) => (
-                <Field data-invalid={!!fieldState.error}>
-                  <FieldLabel htmlFor="password">Password</FieldLabel>
+          {/* SUBMIT */}
+          <Field orientation="horizontal">
+            <Button
+              disabled={loading}
+              type="submit"
+              className="w-full h-14 text-base rounded-none"
+            >
+              {loading ? "Submitting…" : "Submit"}
+            </Button>
+          </Field>
 
-                  <FormControl>
-                    <Input
-                      id="password"
-                      type="password"
-                      placeholder="••••••"
-                      variant="primary"
-                      className="h-14"
-                      {...field}
-                    />
-                  </FormControl>
+          {/* SIGN-UP LINK */}
+          <p className="text-sm text-center mt-2">
+            Don&apos;t have an{" "}
+            <Link href="/sign-up" className="text-primary hover:underline">
+              account?
+            </Link>
+          </p>
 
-                  <FieldError errors={fieldState.error ? [fieldState.error] : []} />
-                </Field>
-              )}
-            />
-
-            {/* SUBMIT */}
-            <Field orientation="horizontal">
-              <Button disabled={loading} type="submit" className="w-full h-14 text-base rounded-none">
-                {loading ? "Submitting…" : "Submit"}
-              </Button>
-            </Field>
-
-            <p className="text-sm text-center">Don&apos;t have an <Link href="/sign-up" className="text-primary hover:underline">account?</Link></p>
-
-
-          </FieldGroup>
-        </FieldSet>
-      </form>
-    </Form>
+        </FieldGroup>
+      </FieldSet>
+    </form>
   );
 }
--- components/pages/sign-up/sign-up-form.tsx
+++ components/pages/sign-up/sign-up-form.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -18,17 +18,23 @@ import { Button } from "@/components/ui/button";
 import { SignUpFormSchema } from "@/lib/zod/schema.zod";
 import Link from "next/link";
 import toast from "react-hot-toast";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 export default function SignUpForm() {
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<z.infer<typeof SignUpFormSchema>>({
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const form = useForm<z.infer<typeof SignUpFormSchema>>({
     resolver: zodResolver(SignUpFormSchema),
-    defaultValues: { name: "", email: "", password: "" },
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
   });
 
   async function onSubmit(values: z.infer<typeof SignUpFormSchema>) {
@@ -40,63 +46,87 @@ export default function SignUpForm() {
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-80 w-full mx-auto"
-    >
+    <form onSubmit={form.handleSubmit(onSubmit)} className="max-w-80 w-full mx-auto">
       <FieldSet>
         <FieldGroup>
 
           {/* NAME */}
-          <Field data-invalid={!!errors.name}>
-            <FieldLabel htmlFor="name">Name</FieldLabel>
+          <Controller
+            name="name"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="name">Name</FieldLabel>
 
-            <Input
-              id="name"
-              placeholder="John Doe"
-              variant="primary"
-              className="h-14"
-              {...register("name")}
-            />
+                <Input
+                  id="name"
+                  placeholder="John Doe"
+                  variant="primary"
+                  className="h-14"
+                  {...field}
+                />
 
-            
-              <FieldError errors={[errors.name]} />
-          </Field>
+                <FieldError errors={[fieldState.error]} />
+              </Field>
+            )}
+          />
 
           {/* EMAIL */}
-          <Field data-invalid={!!errors.email}>
-            <FieldLabel htmlFor="email">Email</FieldLabel>
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
 
-            <Input
-              id="email"
-              placeholder="example123@xyz.com"
-              variant="primary"
-              className="h-14"
-              {...register("email")}
-            />
+                <Input
+                  id="email"
+                  placeholder="example123@xyz.com"
+                  variant="primary"
+                  className="h-14"
+                  {...field}
+                />
 
-            {errors.email && (
-              <FieldError>{errors.email.message}</FieldError>
+                <FieldError errors={[fieldState.error]} />
+              </Field>
             )}
-          </Field>
+          />
 
           {/* PASSWORD */}
-          <Field data-invalid={!!errors.password}>
-            <FieldLabel htmlFor="password">Password</FieldLabel>
+          <Controller
+            name="password"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field data-invalid={fieldState.invalid}>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
 
-            <Input
-              id="password"
-              type="password"
-              placeholder="••••••"
-              variant="primary"
-              className="h-14"
-              {...register("password")}
-            />
+                <div className="relative w-full">
+                  <Input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder={showPassword ? "Password" : "••••••"}
+                    variant="primary"
+                    className="h-14 pr-12"
+                    {...field}
+                  />
 
-            {errors.password && (
-              <FieldError>{errors.password.message}</FieldError>
+                  <Button
+                    type="button"
+                    variant="link"
+                    onClick={togglePasswordVisibility}
+                    className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
+                  >
+                    {showPassword
+                      ? <EyeOffIcon className="h-5 w-5 text-muted-foreground" />
+                      : <EyeIcon className="h-5 w-5 text-muted-foreground" />
+                    }
+                  </Button>
+                </div>
+
+                <FieldError errors={[fieldState.error]} />
+              </Field>
             )}
-          </Field>
+          />
 
           {/* SUBMIT BUTTON */}
           <Field orientation="horizontal">
@@ -111,14 +141,7 @@ export default function SignUpForm() {
 
           {/* SIGN-IN LINK */}
           <p className="text-sm text-center">
-            Already have an{" "}
-            <Link
-              href="/sign-in"
-              className="text-primary hover:underline"
-            >
-              account?
-            </Link>
-            ?
+            Already have an <Link href="/sign-in" className="text-primary hover:underline">account?</Link>
           </p>
 
         </FieldGroup>
